987ee7bee869655abbb315e55d01735b931a6b1e
Iidqqq <hakuhakushinshiro+github@gmail.com>
2020-03-07 15:09:30 +0900

    :dog:

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,14 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import alias from '@rollup/plugin-alias';
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    dir: 'dist',
-    format: 'cjs',
+    dir: "dist",
+    format: "cjs"
   },
   plugins: [
-    typescript({ include: ['*.(t|j)s+(|x)', '**/*.ts+(|x)'] }),
-    resolve(),
-    alias({
-      entries: [{ find: ' @js', replacement: './src' }],
-    }),
-  ],
+    typescript({ include: ["*.(t|j)s+(|x)", "**/*.ts+(|x)"] }),
+    resolve()
+  ]
 };
