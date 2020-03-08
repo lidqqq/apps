00d21f5dded0218090635b4075d61584a0ca6cd6
Iidqqq <hakuhakushinshiro+github@gmail.com>
2020-03-08 17:42:43 +0900

    :dog:

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import run from "@rollup/plugin-run";
 
-export default {
-  input: "src/index.js",
-  output: {
-    dir: "dist",
-    format: "cjs"
-  },
-  plugins: [
-    typescript({ include: ["*.(t|j)s+(|x)", "**/*.ts+(|x)"] }),
-    resolve()
-  ]
+export default watch => {
+  return {
+    input: "src/index.js",
+    output: {
+      dir: "dist",
+      format: "cjs"
+    },
+    plugins: [
+      typescript({ include: ["*.(t|j)s+(|x)", "**/*.ts+(|x)"] }),
+      resolve(),
+      watch && run()
+    ]
+  };
 };
