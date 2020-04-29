d5e3a4c78e3a28511f02345a22a21d3cd8a0372f
Iidqqq <hakuhakushinshiro+github@gmail.com>
2020-04-29 14:58:51 +0900

    :dog:

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ const code = readFileSync("./test/a.html.twig", "utf-8");
 // const code = "{% spaceless %} This is some Twig code {% endspaceless %}";
 const abstractSyntaxTree = parse(code, extension);
 
-console.log(abstractSyntaxTree);
+console.log(JSON.stringify(abstractSyntaxTree, null, 2));
